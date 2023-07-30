041e9ec7f53c57d24ab8cd1588fb7afb8068612c
Román Cárdenas <rcardenas.rod@gmail.com>
2023-07-30 21:14:01 +0200

    Pass instruction instead of pc

--- src/lib.rs
+++ src/lib.rs
@@ -13,24 +13,27 @@ macro_rules! amo {
     };
 }
 
-/// is_atomic_instruction
-/// 
-/// Take the program counter address and returns whether the instruction at that address is an atomic one
-pub unsafe fn is_atomic_instruction(pc: usize) -> bool {
-    (*(pc as *const usize) & 0b1111111) == 0b0101111
+/// Checks if the instruction is an atomic one.
+#[inline(always)]
+pub fn is_atomic_instruction(insn: usize) -> bool {
+    (insn & 0b1111111) == 0b0101111
 }
 
-/// atomic_emulation
+/// Takes the instruction that triggered the exception and an array of
+/// registers at point of exception with [`PLATFORM_REGISTER_LEN`] length.
+/// Returns true if the instruction was atomic and was emulated, false otherwise.
 /// 
-/// Takes the exception program counter and an array of registers at point of exception with [`PLATFORM_REGISTER_LEN`] length.
-pub unsafe fn atomic_emulation(pc: usize, frame: &mut [usize; PLATFORM_REGISTER_LEN]) -> bool {
+/// # Safety
+/// 
+/// This function is supposed to be called right after the instruction caused an exception.
+/// It also assumes that all the user registers were correctly saved and sorted in a trap frame.
+#[inline]
+pub unsafe fn atomic_emulation(insn: usize, frame: &mut [usize; PLATFORM_REGISTER_LEN]) -> bool {
     static mut S_LR_ADDR: usize = 0;
 
-    if !is_atomic_instruction(pc) {
+    if !is_atomic_instruction(insn) {
         return false;
     }
-
-    let insn: usize = *(pc as *const _);
 
     let reg_mask = 0b11111;
     // destination register
